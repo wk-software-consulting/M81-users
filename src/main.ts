f2026fc43437102e3b84e6cb9bdd76e4265eba20
William Koller <williamkoller404@gmail.com>
2022-08-29 21:00:08 -0300

    feat: add swagger and others configs

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@/modules/app/app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { ModelNotFoundException } from './shared/filters/model-not-found.exception.filter';
+import { HttpExceptionFilter } from './shared/filters/http-exception.filter';
+import { swaggerDoc } from './shared/config/doc/swagger-doc';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const logger = new Logger('Main');
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.setGlobalPrefix('api');
+
+  swaggerDoc(app);
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalFilters(new ModelNotFoundException(), new HttpExceptionFilter());
+
   const config = app.get(ConfigService);
   const port = config.get('port');
 
