5e657ad5c61b0c5f52a2a23a9b75a4f0d5298d32
williamkoller404@gmail.com <williamkoller404@gmail.com>
2022-09-20 19:20:43 -0300

    feat: add message pattern

--- src/presentation/controllers/login/login/login-controller.ts
+++ src/presentation/controllers/login/login/login-controller.ts
@@ -1,17 +1,44 @@
-import { AccountModel } from '@app/domain/models/account/account';
 import { AddAccount } from '@app/domain/usecases/account/add-account';
 import { AddAccountDecorator } from '@app/module/account/decorators/add-account.decorator';
 import { AccountDTO } from '@app/presentation/dtos/account/account.dto';
-import { Body, Controller, Post } from '@nestjs/common';
+import { Controller, Logger } from '@nestjs/common';
+import {
+  Ctx,
+  MessagePattern,
+  Payload,
+  RmqContext,
+} from '@nestjs/microservices';
+
+const ackErrors: string[] = ['E11000'];
 
 @Controller('login')
 export class LoginController {
+  private logger = new Logger(LoginController.name);
   constructor(
     @AddAccountDecorator() private readonly addAccountService: AddAccount,
   ) {}
 
-  @Post()
-  async handle(@Body() accountDTO: AccountDTO): Promise<AccountModel> {
-    return await this.addAccountService.add(accountDTO);
+  @MessagePattern('add.account')
+  async handle(
+    @Payload() accountDTO: AccountDTO,
+    @Ctx() ctx: RmqContext,
+  ): Promise<void> {
+    const channel = ctx.getChannelRef();
+    const orginalMessage = ctx.getMessage();
+    this.logger.log(`${JSON.stringify(accountDTO)}`);
+
+    try {
+      await Promise.all([
+        channel.ack(orginalMessage),
+        this.addAccountService.add(accountDTO),
+      ]);
+    } catch (e) {
+      this.logger.log(`${JSON.stringify(e.message)}`);
+      const filterAckError = ackErrors.filter((ackError) =>
+        e.message.includes(ackError),
+      );
+
+      if (filterAckError.length) await channel.ack(orginalMessage);
+    }
   }
 }
